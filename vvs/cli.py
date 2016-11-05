a5e237b7130c3ddce21704670bb64757b04e9179
Aengus Walton <ventolin@gmail.com>
2016-11-05 16:58:59 +0100

    Add list_directions command

--- vvs/cli.py
+++ vvs/cli.py
@@ -10,9 +10,24 @@ URL = ('http://www2.vvs.de/vvs/widget/XML_DM_REQUEST?zocationServerActive=1'
        '&lsShowTrainsExplicit1&stateless=1&language=de&SpEncId=0&anySigWhenPerfectNoOtherMatches=1'
        '&depArr=departure&type_dm=any&anyObjFilter_dm=2&deleteAssignedStops=1&useRealtime=1'
        '&mode=direct&dmLineSelectionAll=1&name_dm={station_id}&itdDateYear={year}&itdDateMonth={month}'
-       '&itdDateDay={day}&itdTimeHour={hour}&itdTimeMinute={minute}&limit=50')
+       '&itdDateDay={day}&itdTimeHour={hour}&itdTimeMinute={minute}&limit={limit}')
 
 TIME_FORMAT = '%Y-%m-%d %H:%M'
+
+
+def search(station_id, limit=50):
+    now = datetime.now()
+    ctx = {
+        'station_id': station_id,
+        'year': now.year,
+        'month': now.month,
+        'day': now.day,
+        'hour': now.hour,
+        'minute': now.minute,
+        'limit': limit
+    }
+    url = URL.format(**ctx)
+    return BeautifulSoup(requests.get(url).content, 'html.parser')
 
 
 @click.group()
@@ -26,17 +41,7 @@ def main():
               help="Filter departures by those in a certain direction")
 def scrape(station_id, direction):
     direction = [d.lower() for d in direction]
-    now = datetime.now()
-    ctx = {
-        'station_id': station_id,
-        'year': now.year,
-        'month': now.month,
-        'day': now.day,
-        'hour': now.hour,
-        'minute': now.minute
-    }
-    url = URL.format(**ctx)
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    soup = search(station_id)
     result = []
     for departure in soup.find_all('itddeparture'):
         if direction and departure.itdservingline['direction'].lower() not in direction:
@@ -48,6 +53,17 @@ def scrape(station_id, direction):
                       int(departure.itddatetime.itdtime['minute']))
         result.append(dt.strftime(TIME_FORMAT))
     click.echo(json.dumps(result))
+
+
+@main.command()
+@click.argument('station_id')
+def list_directions(station_id):
+    soup = search(station_id, limit=500)
+    directions = set()
+    for departure in soup.find_all('itddeparture'):
+        directions.add(departure.itdservingline['direction'])
+    for direction in sorted(list(directions)):
+        click.echo(direction)
 
 
 @main.command()
